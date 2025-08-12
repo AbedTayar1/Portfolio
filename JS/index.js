796a06cbd42d0eab04fcfdcd5b31321ad21a39fa
AbedTayar1 <abed.tayar@e.braude.ac.il>
2025-08-12 15:01:11 +0300

    Initial commit

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
 
 
-// Smooth scroll for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
